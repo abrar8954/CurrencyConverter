a31392b068c6151f648775f7779e910c43074adb
Abrar Sardar <abrarsardar8955@gmail.com>
2023-06-05 20:42:40 +0500

    Currency Converter

---
+++ app/src/main/java/com/abrarsardar/app2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.abrarsardar.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textview;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button2);
+        textview = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Submit...", Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                int  dollar = Integer.parseInt(s);
+                double pak_rupees = dollar * 162.59;
+                textview.setText("The Pakistani Rupees are: " + pak_rupees);
+            }
+        });
+
+    }
+}
